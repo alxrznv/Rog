53eac3b0ade7401844b774f3f29d141c341fcb7f
Artem <100172949+Artemvv8856@users.noreply.github.com>
2023-03-21 21:27:38 +1000

    Add files via upload

---
+++ Health.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prot_Ant
+{
+    internal class Health
+    {
+        /// <summary>
+        /// Мощность аптечки 
+        /// </summary>
+        public string Health_power { get; set; }
+        /// <summary>
+        /// Стоимость аптечки у торговца
+        /// </summary>
+        public string Health_cost { get; set; }
+        /// <summary>
+        /// Шанс найти у торговца
+        /// </summary>
+        public string Health_chance { get; set; }
+
+        public Health(string health_power, string health_cost, string health_chance)
+        {
+            Health_power = "30";
+            Health_cost = "2";
+            Health_chance = health_chance;
+        }
+    }
+}
---
+++ Weapon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prot_Ant
+{
+    internal class Weapon
+    {   
+        /// <summary>
+        /// Вид оружия: пистолет = 1, автомат = 2
+        /// </summary>
+        public string Weapon_type { get; set; }
+
+        /// <summary>
+        /// Урон оружия
+        /// </summary>
+        public string Weapon_damage { get; set; }
+        /// <summary>
+        /// Стоимость оружия
+        /// </summary>
+        public string Weapon_cost { get; set; }
+        /// <summary>
+        /// Шанс найти оружие у торговца
+        /// </summary>
+        public string Weapon_chance { get; set; }
+
+     
+
+    }
+}
