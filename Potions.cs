04236b074c88e9df099b8cac7637c3e3980d9989
alxrznv <alexey.ryazanov.77@mail.ru>
2023-03-24 22:36:47 +1000

    Класс зелий

---
+++ Potions.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Rog{
+
+    public class Potion{
+        public int Points {get; set;}
+        public int Cost {get; set;}
+        public Potion(){
+            Points = 20;
+            Cost = 20;
+        }
+    }
+}
