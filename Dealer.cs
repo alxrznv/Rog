4a103b5b8f0031468bae40beed7da90636693830
Plyushik0 <bwx999@mail.ru>
2023-03-26 01:53:01 +1000

    дилер

--- Dealer.cs
+++ Dealer.cs
@@ -3,12 +3,67 @@ using System;
 
 namespace Rog
 {
-
+    
     public class Dealer
     {
-        public int Item_Spawn_Chance { get; set; }
-        public int Meet_Chance { get; set; }
-        public int Documents { get; set; }
-        
+        static int itemChance { get; set; }
+        static int agility { get; set; }
+        static int sharpshooting { get; set; }
+        static int firstAid { get; set; }
+        static int documents { get; set; }
+        static int race { get; set; }
+        static int sharpshootingPotion { get; set; }
+        static int chemistryAmmount { get; set; }
+        static int AgilityPotionAmmount { get; set; }
+        static int SharpshootingPotionAmmount { get; set; }
+        public static void LoadDealer(Player p)
+
+        {
+            
+            
+            StartDialogue(p);
+        }
+
+        public static void StartDialogue(Player p)
+        {
+            int chemistryAmmountD
+            int agilityPotionAmmountD
+            race = p.race;
+            int sharpshootingD;
+            int firstAidD = p.firstAid;
+            documents = p.documents;
+            while (true) 
+            {
+                if (p.race == 1)
+                {   
+                    Console.Clear()
+                    Console.WriteLine("Добро пожаловать в мой магазин, путник! Товара не много, так как он очень редкий и ,соответственно, дорогой, так что выбирай с умом!");
+                    Console.WriteLine("|        ЛАВКА           |");
+                    Console.WriteLine("+=+=+=+=+=+=+=+=+=+=+=+=+=");
+                    Console.WriteLine("|    Зелье ловкости(1)   |");
+                    Console.WriteLine("|    Зелье меткости(2)   |");
+                    Console.WriteLine("|        Аптечка(3)      |");
+                    Console.WriteLine("|        Автомат(4)      |");
+                    Console.WriteLine("+=+=+=+=+=+=+=+=+=+=+=+=+=");
+
+                }
+
+                else
+                {
+                    Console.WriteLine("Добро пожаловать в мой магазин, путник! Товара не много, так как он очень редкий и ,соответственно, дорогой, так что выбирай с умом!");
+                    Console.WriteLine("|         ЛАВКА        |");
+                    Console.WriteLine("+=+=+=+=+=+=+=+=+=+=+=+=");
+                    Console.WriteLine("|       Аптечка(1)     |");
+                    Console.WriteLine("|       Автомат(2)     |");
+                    Console.WriteLine("+=+=+=+=+=+=+=+=+=+=+=+=");
+
+                }
+
+            }
+            
+            
+
+
+        }
     }
 }
--- Player.cs
+++ Player.cs
@@ -3,14 +3,42 @@ using System;
 namespace Rog{
 
     public class Player{
+        Random rand;
         public String Name {get; set;}
         public int Race {get; set;}
         public int Health{get; set;}
-         public int Sharpshooting {get; set;}
+        public int Sharpshooting {get; set;}
         public int Agility {get; set;}
         public int Damage {get; set;}
         public int ChemistryAmmount {get; set;}
         public int AgilityPotionAmmount {get; set;}
         public int SharpshootingPotionAmmount {get; set;}
+        public int Item { get; set;}
+        public int Documents { get; set; }
+
+        public int UpHealth()
+        {
+            int upper = (2 * Item + 5)
+            int lower = (Item + 2)
+            return rand.Next(lower, upper);
+        }
+        public int UpAgility()
+        {
+            int upper = (2 * Item + 5)
+            int lower = (Item + 2)
+            return rand.Next(lower, upper);
+        }
+        public int UpSharpshooting()
+        {
+            int upper = (2 * Item + 5)
+            int lower = (Item + 2)
+            return rand.Next(lower, upper);
+        }
+ 
+
+
+
     }
+
+
 }
