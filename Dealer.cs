c8a75374349fdd2a88037ffc0ed92804fc71bf61
Plyushik0 <bwx999@mail.ru>
2023-03-26 01:48:18 +1000

    некоторые дополнения и наработки

---
+++ Dealer.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+namespace Rog
+{
+
+    public class Dealer
+    {
+        public int Item_Spawn_Chance { get; set; }
+        public int Meet_Chance { get; set; }
+        public int Documents { get; set; }
+        
+    }
+}
