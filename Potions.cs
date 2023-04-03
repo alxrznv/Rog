ea1563e18b7cd4ad47736acb81efe6868cd4b7b0
Plyushik0 <bwx999@mail.ru>
2023-04-04 01:05:57 +1000

    доп

---
+++ Potions.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Rog
+{
+
+	public class Potion
+	{
+		public int Points { get; set; }
+		public int Cost { get; set; }
+		public int Duration { get; set; }
+		public Potion()
+		{
+			Points = 20;
+			Cost = 20;
+			Duration = 0;
+		}
+	}
+}
---
+++ Weapon.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Rog
+{
+
+    public class Weapon
+    {
+        public int Damage { get; set; }
+        public int Cost { get; set; }
+        public Weapon(int damage, int cost)
+        {
+            Damage = damage;
+            Cost = cost;
+        }
+    }
+}
